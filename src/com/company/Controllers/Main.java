80303bfe43fa084c19059692138d28bffd146bf2
Michel <michelv1994@caiway.net>
2017-03-08 20:43:54 +0100

    Made a start of the program with Scanner for human input

--- src/com/company/Controllers/Main.java
+++ src/com/company/Controllers/Main.java
@@ -1,10 +1,24 @@
 package com.company.Controllers;
 
+import java.util.Scanner;
+
 public class Main {
 
+    private static int TOTAL_CLUSTERS;
+    private static int TOTAL_ITERATIONS;
+
     public static void main(String[] args) {
-	// write your code her
-        System.out.println("Hello");
-        System.out.println("Hallo Donovan");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many clusters do you want: ");
+        TOTAL_CLUSTERS = scanner.nextInt();
+        System.out.println("How many iterations do you want: ");
+        TOTAL_ITERATIONS = scanner.nextInt();
+        System.out.println("You have chosen for " + TOTAL_CLUSTERS + " clusters and " + TOTAL_ITERATIONS + " iterations");
+
+        /*while (TOTAL_ITERATIONS > 0)
+        {
+            TOTAL_ITERATIONS--;
+        }*/
     }
 }
