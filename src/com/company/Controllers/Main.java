4a5810cd00ac1900d7c7bd65b40091b24e0e74f4
Donovan Martodikromo <donovan.dcm@gmail.com>
2017-03-08 19:56:27 +0100

    Start up

---
+++ src/com/company/Controllers/Main.java
@@ -0,0 +1,10 @@
+package com.company.Controllers;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code her
+        System.out.println("Hello");
+        System.out.println("Hallo");
+    }
+}
