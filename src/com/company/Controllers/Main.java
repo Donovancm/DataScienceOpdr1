835a6d8d76bb228f3b3e0df8b20fbf2d51abaf79
Michel <michelv1994@caiway.net>
2017-03-08 20:20:56 +0100

    Change

--- src/com/company/Controllers/Main.java
+++ src/com/company/Controllers/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code her
         System.out.println("Hello");
-        System.out.println("Hallo");
+        System.out.println("Hallo Donovan");
     }
 }
